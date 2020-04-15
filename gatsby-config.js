006414a507621d3af26ed616887a8d0faa22809f
barisececelik <barisececelik@gmail.com>
2020-04-15 22:21:53 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,10 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    module.exports = {
+      plugins: ['gatsby-theme-overreacted-toggle'],
+  }
+  
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
